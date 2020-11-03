e8d31f70ccb22c936d91804fc05d99d6bd49e13e
alugocp <alugocp@aim.com>
2020-11-02 23:11:52 -0500

    Navbar and organizational changes

---
+++ script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+  function setView(view){
+    $(".view").hide();
+    $(view).show()
+      .css("opacity","0")
+      .animate({"opacity":"1"},200);
+  }
+
+  $(".icon-home").click(() => setView(".home-view"));
+  $(".icon-contents").click(() => setView(".contents-view"));
+  setView(".home-view");
+
+});
